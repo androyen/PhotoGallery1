f1074f6bf5ba69d1e52ceb768244761d6e3e065c
Androyen <androyen@aol.com>
2014-11-13 17:27:58 -0600

    Implement setupAdapter in onPostExecute()

--- PhotoGalleryFragment.java
+++ PhotoGalleryFragment.java
@@ -8,9 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by rnguyen on 11/13/14.
@@ -20,6 +22,7 @@ public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = PhotoGalleryFragment.class.getSimpleName();
 
     GridView mGridView;
+    ArrayList<GalleryItem> mItems;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,14 +41,29 @@ public class PhotoGalleryFragment extends Fragment {
 
         mGridView = (GridView)v.findViewById(R.id.gridView);
 
+        //Set up GridView adapter to persist across device rotation
+        setupAdapter();
         return v;
     }
 
+    void setupAdapter() {
+        if (getActivity() == null || mGridView == null) {
+            return;
+        }
+
+        if (mItems != null) {
+            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+        }
+        else {
+            mGridView.setAdapter(null);
+        }
+    }
+
     //AsyncTask to run network on the background thread
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+    private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {
 //            try {
 //                String result = new FlickrFetchr().getUrl("http://www.google.com");
 //                Log.i(TAG, "Fetched contents of URL: " + result);
@@ -54,8 +72,14 @@ public class PhotoGalleryFragment extends Fragment {
 //                Log.e(TAG, "Failed to fetch URL: " + e);
 //            }
 
-            new FlickrFetchr().fetchItems();
-            return null;
+            return new FlickrFetchr().fetchItems();
+
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems = items;
+            setupAdapter(); // Update the main UI thread of list of Flickr objects
         }
     }
 
