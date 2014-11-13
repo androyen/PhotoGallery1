db58660460a463fc36e5d8b0f37e08b526ecdf0d
Androyen <androyen@aol.com>
2014-11-13 17:05:42 -0600

    Implement FetchItems()

--- FlickrFetchr.java
+++ FlickrFetchr.java
@@ -1,5 +1,8 @@
 package com.androyen.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +13,13 @@ import java.net.URL;
  * Created by rnguyen on 11/13/14.
  */
 public class FlickrFetchr {
+
+    public static final String TAG = FlickrFetchr.class.getSimpleName();
+    private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+    private static final String API_KEY = "93f326b4268077daea2d034d51be6434";
+    private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String PARAM_EXTRAS = "extras";
+    private static final String EXTRA_SMALL_URL = "url_s";
 
     byte[] getUrlBytes(String urlSpec) throws IOException {
 
@@ -43,5 +53,22 @@ public class FlickrFetchr {
 
     public String getUrl(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec)); //Returns string from Flickr website
+    }
+
+    void fetchItems() {
+
+        try {
+            String url = Uri.parse(ENDPOINT).buildUpon()
+                    .appendQueryParameter("method", METHOD_GET_RECENT)
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                    .build().toString();
+
+            String xmlString = getUrl(url);
+            Log.i(TAG, "Received xml: " + xmlString);
+        }
+        catch (IOException e) {
+            Log.e(TAG, "Failed to fetch items", e);
+        }
     }
 }
